3b921ccc463ffdeab666832bbf0b266adf759b29
Adrian Lactuan <aplactuan@gmail.com>
2021-08-30 11:00:19 +0800

    Add product variation type to product variations

--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVariation extends Model
 {
     use HasFactory;
+
+    public function type()
+    {
+        return $this->hasOne(ProductVariationType::class, 'id', 'product_variation_type_id');
+    }
 }
