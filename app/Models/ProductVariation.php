5dee53e6dc377861bccd2f0afc18a18432eb0b2d
Adrian Lactuan <aplactuan@gmail.com>
2021-09-03 18:29:25 +0800

    Set product variation prices and add test

--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use App\Cart\Money;
+use App\Models\Traits\HasPrice;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductVariation extends Model
 {
-    use HasFactory;
+    use HasFactory, HasPrice;
+
+    public function getPriceAttribute($value)
+    {
+        if ($value === null){
+            return $this->product->price;
+        }
+        return new Money($value);
+    }
+
+    public function priceVaries()
+    {
+        return $this->product->price->amount() !== $this->price->amount();
+    }
 
     public function type()
     {
--- tests/Unit/Products/ProductVariationTest.php
+++ tests/Unit/Products/ProductVariationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Products;
 
+use App\Cart\Money;
 use App\Models\Product;
 use App\Models\ProductVariation;
 use App\Models\ProductVariationType;
@@ -21,5 +22,49 @@ class ProductVariationTest extends TestCase
         $variation = ProductVariation::factory()->create();
 
         $this->assertInstanceOf(Product::class, $variation->product);
+    }
+
+    public function test_it_return_a_money_instance_for_the_price_variation()
+    {
+        $variation = ProductVariation::factory()->create();
+
+        $this->assertInstanceOf(Money::class, $variation->price);
+    }
+
+    public function test_it_returns_a_formatted_price()
+    {
+        $variation = ProductVariation::factory()->create([
+            'price' => 1000
+        ]);
+
+        $this->assertEquals($variation->formatted_price, '£10.00');
+    }
+
+    public function test_it_returns_the_base_product_price_if_variation_price_is_null()
+    {
+        $product = Product::factory()->create([
+            'price' => 1000
+        ]);
+
+        $variation = ProductVariation::factory()->create([
+            'price' => null,
+            'product_id' => $product->id
+        ]);
+
+        $this->assertEquals($variation->formatted_price, $product->formatted_price);
+    }
+
+    public function test_it_can_check_if_the_variation_price_is_different_to_the_product()
+    {
+        $product = Product::factory()->create([
+            'price' => 1000
+        ]);
+
+        $variation = ProductVariation::factory()->create([
+            'price' => 2000,
+            'product_id' => $product->id
+        ]);
+
+        $this->assertTrue($variation->priceVaries());
     }
 }
