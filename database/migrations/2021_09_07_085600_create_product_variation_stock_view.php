e6fa8cf65ab87b1129c8586eb7d7d32cc528002b
Adrian Lactuan <aplactuan@gmail.com>
2021-09-07 17:01:42 +0800

    Add product stock database view

---
+++ database/migrations/2021_09_07_085600_create_product_variation_stock_view.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateProductVariationStockView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       DB::statement("
+            CREATE VIEW product_variation_stock_view AS
+            SELECT
+                product_variations.product_id AS product_id,
+                product_variations.id AS product_variation_id,
+                COALESCE(SUM(stocks.quantity) - coalesce(SUM(order_product_variation.quantity), 0), 0) AS stock,
+                case when COALESCE(SUM(stocks.quantity) - coalesce(SUM(order_product_variation.quantity), 0), 0) > 0
+                    then true
+                    ELSE false
+                END in_stock
+            FROM product_variations
+            LEFT JOIN (
+                SELECT
+                        stocks.product_variation_id AS id,
+                        SUM(stocks.quantity) AS quantity
+                FROM stocks
+                GROUP BY stocks.product_variation_id
+            ) AS stocks USING (id)
+            LEFT JOIN (
+                SELECT
+                    order_product_variation.product_variation_id AS id,
+                    SUM(order_product_variation.quantity) AS quantity
+                FROM order_product_variation
+                GROUP BY order_product_variation.product_variation_id
+            ) AS order_product_variation USING (id)
+            GROUP BY product_variations.id
+       ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("DROP VIEW if EXISTS product_variation_stock_view");
+    }
+}
